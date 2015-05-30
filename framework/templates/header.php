80b5e85ae6d1b681ed7ebac0105238bc129d20b5
thynkbig <thynkbig@yahoo.com>
2015-05-29 23:54:36 -0500

    Added Header, Index, and Post content/styling

---
+++ framework/templates/header.php
@@ -0,0 +1,12 @@
+<div id="header">
+	<div id="logo">
+		<a href="<?php echo bloginfo('url') ?>"><p>
+			vanderbilthustler
+		</p></a>
+	</div>
+	<div id="social-icons">
+		<a href="http://www.facebook.com"><img src="<?php echo bloginfo('url') . '/wp-content/uploads/2015/05/Facebook_logo.png'; ?>" alt="Facebook"></img></a>
+		<a href="http://www.twitter.com"><img src="<?php echo bloginfo('url') . '/wp-content/uploads/2015/05/Twitter_logo.png'; ?>" alt="Twitter"></img></a>
+		<a href="http://www.instagram.com"><img src="<?php echo bloginfo('url') . '/wp-content/uploads/2015/05/instagram_logo.png'; ?>" alt="Instagram"></img></a>
+	</div>
+</div>
--- functions.php
+++ functions.php
@@ -6,3 +6,36 @@ function avada_child_scripts() {
 	}
 }
 add_action('wp_enqueue_scripts', 'avada_child_scripts');
+
+
+if ( ! function_exists('homepage_list')) {
+	function homepage_list() {
+		echo '<div class="list">';
+			$args = array (	'category_name'      => 'feature-stories' );
+			$stories = new WP_Query( $args );
+
+			if ( $stories->have_posts()) {
+				echo '<ul class="post-list">';
+
+					while ( $stories->have_posts() ) {
+
+						$stories->the_post();
+							echo '<div class="stuff"><li class="post-list-item">';
+							if (has_post_thumbnail( $post->ID )) {
+								$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+									<a class="link" href="<?php echo get_permalink() ?>">
+									<div class="post-info" style="background-image: url('<?php echo $image[0]; ?>')">
+										<div id="text"><p><b><?php echo get_the_title($post->ID); ?></b></p></div>
+									</div>
+									</a>
+								
+								<?php }
+							echo '</li></div>';
+						
+					}
+
+				echo '</ul>';
+			}
+		echo '</div>';
+	}
+}
