3316731592d9cbf9c0e187372f3c4a49cc76fe60
thynkbig <thynkbig@yahoo.com>
2015-09-22 16:11:31 -0500

    Fixed stylesheets after temp fixes, and bugfixed single.php to make ACFs optional

--- single.php
+++ single.php
@@ -166,11 +166,13 @@
 			<?php endif; // 2 ?>
 			<?php endif; // 1 ?>
 			<?php if($smof_data['blog_post_title']): ?>
-
 			
 			
 			<?php if( get_field('headline') ): ?>
-				<div class="entry-title"><h2><?php the_field('headline'); ?></h2></div>
+			<h2<?php if( ! $smof_data['disable_date_rich_snippet_pages'] ) { echo ' class="entry-title"'; } ?>><?php the_field('headline'); ?></h2>
+			<?php elseif( ! $smof_data['disable_date_rich_snippet_pages'] ): ?>
+			<span class="entry-title" style="display: none;"><?php the_field('headline'); ?></span>
+			<?php endif; ?>
 			<?php endif; ?>
 			
 			<?php if( get_field('deck') ): ?>
